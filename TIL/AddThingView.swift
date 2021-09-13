b03f777defd36aea821180fdb9076efc754b97e3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-09-13 11:07:12 +0800

    indent and add binding to addThingView

--- TIL/AddThingView.swift
+++ TIL/AddThingView.swift
@@ -33,20 +33,21 @@
 import SwiftUI
 
 struct AddThingView: View {
-  @Environment(\.presentationMode) var presentationMode
-
-  var body: some View {
-    VStack {
-      Button("Done") {
-        presentationMode.wrappedValue.dismiss()
-      }
-      Spacer()
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var someThings: ThingStore
+    
+    var body: some View {
+        VStack {
+            Button("Done") {
+                presentationMode.wrappedValue.dismiss()
+            }
+            Spacer()
+        }
     }
-  }
 }
 
 struct AddThingView_Previews: PreviewProvider {
-  static var previews: some View {
-    AddThingView()
-  }
+    static var previews: some View {
+        AddThingView()
+    }
 }
